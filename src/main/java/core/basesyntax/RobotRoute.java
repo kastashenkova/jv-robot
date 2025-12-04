a8d2bffac2bb18218a32ff6399804f9410e7da6a
kastashenkova <astashenkova.katya@gmail.com>
2025-12-04 02:44:46 +0200

    found way for robot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,35 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        findRightX(robot, toX);
+        findRightY(robot, toY);
+    }
+
+    private void findRightX(Robot robot, int toX) {
+        while (robot.getX() != toX) {
+            if (robot.getX() > toX) {
+                findRightDirection(robot, Direction.LEFT);
+            } else {
+                findRightDirection(robot, Direction.RIGHT);
+            }
+            robot.stepForward();
+        }
+    }
+
+    private void findRightY(Robot robot, int toY) {
+        while (robot.getY() != toY) {
+            if (robot.getY() > toY) {
+                findRightDirection(robot, Direction.UP);
+            } else {
+                findRightDirection(robot, Direction.DOWN);
+            }
+            robot.stepForward();
+        }
+    }
+
+    private void findRightDirection(Robot robot, Direction correct) {
+        while (robot.getDirection() != correct) {
+            robot.turnLeft();
+        }
     }
 }
